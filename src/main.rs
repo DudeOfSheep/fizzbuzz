7a2ac1ea0472d6e9727a73460d864ab12baedd4c
Mudshoes <kg082108@wgcloud.org>
2025-08-08 19:09:26 -0500

    Functional FizzBuzz

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::io;
+
+fn main() {
+    let mut number: i32 = 0;
+
+    println!(
+        "<-- FizzBuzz! -->
+        Starting from 1, keep counting with the computer until failure.
+        If the next number is a multiple of 3, say Fizz instead.
+        If the next number is a multiple of 5, say Buzz.
+        If your number is a multiple of 3 and 5, say FizzBuzz!
+        
+        0"
+    );
+
+    loop {
+        // User Input
+        let mut input: String = String::new();
+        number += 1;
+
+        match io::stdin()
+            .read_line(&mut input) {
+                Ok(_) => (),
+                Err(n) => { 
+                    println!("Input Error: {n}"); 
+            }
+        }
+
+        if !(input.trim().to_lowercase() == check_answer(number,"").to_lowercase()) {
+            println!("Gotcha! It was {}!", check_answer(number,"!"));
+            break;
+        }
+
+        number += 1;
+        println!("{}",check_answer(number,"!"))
+    }
+
+    println!("Press Enter to Exit");
+    io::stdin().read_line(&mut String::new()).expect("Issue Reading Input but I don\'t care");
+}
+
+fn check_answer(num: i32,tack: &str) -> String {
+    let mods: (bool, bool) = (num % 3 == 0, num % 5 == 0);
+    match mods {
+        (false,false) => num.to_string(),
+        (true,false) => String::from("Fizz"),
+        (false,true) =>  String::from("Buzz"),
+        (true,true) => String::from(format!("FizzBuzz{tack}")),
+    }
+}
