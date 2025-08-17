ff5f5eda51576586140d06cc89cab825dba375d6
Mudshoes <kg082108@wgcloud.org>
2025-08-17 02:59:13 -0500

    Optimized it more

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
+use std::{error::Error, io};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut number: i32 = 0;
 
     println!(
@@ -18,30 +18,31 @@ fn main() {
         let mut input: String = String::new();
 
         number += 1;
-        let Ok(_) = io::stdin().read_line(&mut input) else {
-            return;
-        };
+        io::stdin().read_line(&mut input)?;
 
-        if !(input.trim().to_lowercase() == check_answer(number, "").to_lowercase()) {
-            println!("Gotcha! It was {}!", check_answer(number, "!"));
+        // Lowercase last so we don't check if we can lowercase the '!'. Optimizations are Critical here.
+        if !(input.trim().to_lowercase() == check_answer(number).strip_suffix("!").unwrap().to_lowercase()) {
+            println!("Gotcha! It was {}!", check_answer(number));
             break;
         };
 
-        number += 1;
-        println!("{}", check_answer(number, "!"))
+        if let None = number.checked_add(1) { println!("I\'m tired... I give up... You win..."); break; }
+        println!("{}", check_answer(number))
     }
 
     println!("Press Enter to Exit");
     io::stdin()
         .read_line(&mut String::new())
         .expect("Issue Reading Input but I don\'t care");
+
+    Ok(())
 }
 
-fn check_answer(num: i32, tack: &str) -> String {
+fn check_answer(num: i32) -> String {
     match (num % 3 == 0, num % 5 == 0) {
         (false, false) => num.to_string(),
         (true, false) => String::from("Fizz"),
         (false, true) => String::from("Buzz"),
-        (true, true) => String::from(format!("FizzBuzz{tack}")),
+        (true, true) => String::from(format!("FizzBuzz!")),
     }
 }
